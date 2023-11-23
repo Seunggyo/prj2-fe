97b8c0ae69aac51964b99c142ed0b9c95e768a49
hrhrhrasd <gmleh1245@gmail.com>
2023-11-23 17:51:27 +0900

    src/page/member/MemberLogin.js, MemberSignup.js 생성만

---
+++ src/page/member/MemberSignup.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+function MemberSignup(props) {
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div></div>
+    );
+}
+
+export default MemberSignup;
