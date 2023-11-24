fafb63ff2172a9f29bfcc52385abfe7ffe06ff4c
Seunggyo <tmdry99@gmail.com>
2023-11-24 11:31:18 +0900

    값 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import {HomeLayout} from "./layout/HomeLayout";
-import React from "react";
+import React, {createContext, useEffect, useState} from "react";
 import {Drug} from "./page/Drug";
 import {Box} from "@chakra-ui/react";
 import {Hs} from "./page/Hs";
+import axios from "axios";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -16,12 +17,23 @@ const router = createBrowserRouter(
 
 
 function App() {
+    const [key, setKey] = useState(null)
+    useEffect(() => {
+        axios.get("/hospital")
+            .then(response => setKey(response.data));
+    }, []);
+
     return (
         <Box>
-            <RouterProvider router={router}/>
+            <KakaoMapAppContext.Provider value={key}>
+                <RouterProvider router={router}/>
+            </KakaoMapAppContext.Provider>
+
 
         </Box>
     );
 }
+
+export const KakaoMapAppContext = createContext(null)
 
 export default App;
