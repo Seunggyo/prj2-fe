493959f1e98a9dccc966ddcf1db0dd07d573d814
som42 <zzzoo42@naver.com>
2023-11-24 14:21:27 +0900

    DrugWrite 폴더 안에 기능성 영양제 버튼 하나씩 다 만듬!

--- src/page/DrugWrite.js
+++ src/page/DrugWrite.js
@@ -1,10 +1,63 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Stack,
+  StackDivider,
+} from "@chakra-ui/react";
 
 export function DrugWrite() {
   return (
     <Box>
-      <h1>기능성 영양제</h1>
-      <Box></Box>
+      <Box width="100%" height="50px">
+        <h1>기능성 영양제</h1>
+      </Box>
+      <Flex>
+        <Box width="200px" height="250px" border="1px solid #ffd6d1">
+          <Box height="100px" width="100px" margin="50px">
+            <img src="/images/firstScreen/up.jpg" height="100%" width="100%" />
+            <h1>위 건강</h1>
+          </Box>
+        </Box>
+        <Box width="200px" height="250px" border="1px solid #ffd6d1">
+          <Box height="100px" width="100px" margin="50px">
+            <img src="/images/firstScreen/i.jpg" height="100%" width="100%" />
+            <h1>눈 건강</h1>
+          </Box>
+        </Box>
+        <Box width="200px" height="250px" border="1px solid #ffd6d1">
+          <Box height="100px" width="100px" margin="50px">
+            <img
+              src="/images/firstScreen/Liver.jpg"
+              height="100%"
+              width="100%"
+            />
+            <h1>간 건강</h1>
+          </Box>
+        </Box>
+        <Box width="200px" height="250px" border="1px solid #ffd6d1">
+          <Box height="100px" width="100px" margin="50px">
+            <img src="/images/firstScreen/ti.jpg" height="100%" width="100%" />
+            <h1>피로 개선</h1>
+          </Box>
+        </Box>
+        <Box width="200px" height="250px" border="1px solid #ffd6d1">
+          <Box height="100px" width="100px" margin="50px">
+            <img src="/images/firstScreen/ch.jpg" height="100%" width="100%" />
+            <h1>어린이 성장</h1>
+          </Box>
+        </Box>
+        <Box width="200px" height="250px" border="1px solid #ffd6d1">
+          <Box height="200px" width="100px" margin="50px">
+            <img src="/images/firstScreen/sel.jpg" height="100%" width="100%" />
+            <h1>수면질 개선</h1>
+          </Box>
+        </Box>
+      </Flex>
     </Box>
   );
 }
