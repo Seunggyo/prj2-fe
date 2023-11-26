d71cf9ac98e07fe5c96aa5f77462a66a91a6f428
hwani <klh481@naver.com>
2023-11-26 20:28:24 +0900

    front boardwrite
    dongul, gowun폰트 추가설정 및
    boardwrite 생성..

--- src/page/board/BoardWrite.js
+++ src/page/board/BoardWrite.js
@@ -1,5 +1,13 @@
-import { useState } from "react";
-import { useToast } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Box,
+  Card,
+  Flex,
+  FormLabel,
+  Select,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -44,61 +52,79 @@ export function BoardWrite() {
       .finally(() => setIsSubmitting(false));
   }
 
+  function setAuth(value) {}
+
   return (
     <div>
       <form>
         <div className="bg-indigo-50 min-h-screen md:px-20 pt-6">
-          <div className=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
-            <h1 className="text-center text-2xl font-bold text-gray-500 mb-10">
+          <div className=" bg-white rounded-md px-6 py-10 max-w-4xl mx-auto">
+            <h1 className="text-center text-6xl font-dongle text-gray-500 mb-10">
               게시판 글 작성
             </h1>
             <div className="space-y-4">
               <div>
-                <label htmlFor="title" className="text-lx">
-                  제목:
-                </label>
+                <span className="font-dongle text-4xl text-gray-500">
+                  제 목:
+                </span>
                 <input
                   type="text"
                   id="title"
-                  placeholder="title"
-                  className="ml-2 outline-none py-1 p-2 w-4/5 text-md border-2 rounded-md"
+                  placeholder="Title"
+                  className="ml-2 outline-none py-1 p-2 w-3/5 text-md border-2 rounded-md"
                 />
               </div>
               <div>
-                <label
-                  htmlFor="description"
-                  className="block mb-2 text-lg"
-                ></label>
                 <textarea
                   id="description"
                   cols="30"
                   rows="10"
-                  className="w-full p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"
+                  className="w-full p-4 text-gray-  600 bg-indigo-50 outline-none rounded-md"
                 ></textarea>
               </div>
-              <div>
-                <label htmlFor="name" className="text-lx">
-                  작성자:
-                </label>
-                <input
-                  type="text"
-                  placeholder="NickName"
-                  id="name"
-                  className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
-                />
+              <div className="flex space-x-24">
+                <div>
+                  <span className="font-dongle text-4xl text-gray-500">
+                    작성자:
+                  </span>
+                  <input
+                    type="text"
+                    placeholder="NickName"
+                    id="name"
+                    className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+                  />
+                </div>
+                <div>
+                  <Flex>
+                    <span className="font-dongle text-4xl text-gray-500">
+                      게시판:
+                    </span>
+                    <Flex ml="4">
+                      <Select
+                        defaultValue={"user"}
+                        onChange={(e) => setAuth(e.target.value)}
+                      >
+                        {/*TODO: 게시판유형 더 추가해야함..*/}
+
+                        <option value={"ds"}>약국</option>
+                        <option value={"hs"}>병원</option>
+                      </Select>
+                    </Flex>
+                  </Flex>
+                </div>
               </div>
               <div>
-                <label htmlFor="email" className="text-lx">
-                  이메일:
-                </label>
+                <span className="font-dongle text-4xl text-gray-500">
+                  첨부파일
+                </span>
                 <input
-                  type="text"
-                  placeholder="e-mail"
-                  id="email"
-                  className="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+                  className="block w-4/5 text-sm text-gray-900 border
+                  border-gray-300 rounded-lg cursor-pointer bg-gray-50"
+                  id="file_input"
+                  type="file"
                 />
               </div>
-              <button class=" px-6 py-2 mx-auto block rounded-md text-lg font-semibold text-indigo-100 bg-indigo-600  ">
+              <button class=" px-8 py-2 mx-auto block rounded-md font-dongle text-3xl text-indigo-100 bg-indigo-600  ">
                 작성 완료
               </button>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      gowun: ["gowun", "sans-serif"],
+      dongle: ["dongle", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
