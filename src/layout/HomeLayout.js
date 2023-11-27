0f89f0540d015b17b2f1bfb3fbcebbfdf66c24e6
hwani <klh481@naver.com>
2023-11-27 20:19:29 +0900

    front board 1차 머지 설정 끝..

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -1,13 +1,13 @@
-import {Flex} from "@chakra-ui/react";
-import {Outlet} from "react-router-dom";
-import {NavBar} from "../component/NavBar";
+import { Flex } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import { NavBar } from "../component/Navbar";
 import React from "react";
 
 export function HomeLayout() {
-    return (
-        <Flex>
-            <NavBar />
-            <Outlet/>
-        </Flex>
-    );
+  return (
+    <Flex>
+      <NavBar />
+      <Outlet />
+    </Flex>
+  );
 }
