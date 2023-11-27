f734c169c142993978942b112532a1d288a2b69c
hrhrhrasd <gmleh1245@gmail.com>
2023-11-27 12:08:21 +0900

    memberList, memberView 추가, memberEdit 작업중

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomeLayout from "./layout/HomeLayout";
 import MemberView from "./page/member/MemberView";
 import LoginProvider from "./component/LoginProvider";
 import MemberLogin from "./page/member/MemberLogin";
+import MemberEdit from "./page/member/MemberEdit";
 
 
 const routes = createBrowserRouter(
@@ -14,6 +15,7 @@ const routes = createBrowserRouter(
             <Route path="member/list" element={<MemberList />} />
             <Route path="member/view" element={<MemberView />} />
             <Route path="member/login" element={<MemberLogin />} />
+            <Route path="member/edit" element={<MemberEdit />} />
         </ Route>
     )
 )
---
+++ src/page/member/MemberEdit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MemberEdit(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default MemberEdit;
