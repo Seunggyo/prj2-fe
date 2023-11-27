0be6e9785b615d42bf982786748f6fd0c034c62a
amz940 <dh_bug@naver.com>
2023-11-27 17:06:45 +0900

    약국 정보 작성 후 약국 리스트로 넘어오게 수정

--- src/page/DrugStore/DsWrite.js
+++ src/page/DrugStore/DsWrite.js
@@ -54,7 +54,7 @@ export function DsWrite() {
           description: "정보가 잘 저장되었습니다",
           status: "success",
         });
-        navigate("/");
+        navigate("/dsList");
       })
       .catch((error) => {
         if (error.response.status === 400) {
