0d5f7702a35ede6ecc3147ae1d2c59205fc883e6
hwani <klh481@naver.com>
2023-11-27 14:19:20 +0900

    front board
    login 제작중..

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -9,6 +9,7 @@ export function Navbar() {
       <Button onClick={() => navigate("/")}>메인화면</Button>
       <Button onClick={() => navigate("/write")}>글쓰기</Button>
       <Button onClick={() => navigate("/login")}>로그인</Button>
+      <Button onClick={() => navigate("/member/signup")}>회원가입</Button>
     </Flex>
   );
 }
--- src/page/member/MemberLogin.js
+++ src/page/member/MemberLogin.js
@@ -1,3 +1,133 @@
+import { useContext, useState } from "react";
+import { useToast } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { LoginContext } from "../../component/LoginProvider";
+
 export function MemberLogin(props) {
-  return <h1>로그인</h1>;
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const { fetchLogin } = useContext(LoginContext);
+
+  function handleLogin() {
+    axios
+      .post("/api/member/login", { id, password })
+      .then(() => {
+        toast({
+          description: "로그인 되었습니다.",
+          status: "success",
+        });
+        navigate("/");
+      })
+      .catch(() => {
+        toast({
+          description: "아이디와 암호를 다시 확인해주세요.",
+          status: "warning",
+        });
+      })
+      .finally(() => {
+        fetchLogin();
+      });
+  }
+
+  return (
+    <div className="bg-white dark:bg-gray-900">
+      <div className="flex justify-center h-screen">
+        <div
+          className="hidden bg-cover lg:block lg:w-2/3"
+          // style={{
+          //   backgroundImage: `url(" ./ assets / images / 샘플용.jpg")`,
+          // }}
+          //TODO: 왜 백그라운드 이미지가 안되지?
+        >
+          <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+            <div>
+              <h2 className="text-4xl font-bold text-white">아프지마!</h2>
+
+              <p className="max-w-xl mt-3 text-gray-300"></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+          <div className="flex-1">
+            <div className="text-center">
+              <h2 className="text-4xl font-bold text-center text-gray-700 dark:text-white">
+                아프지마
+              </h2>
+
+              <p className="mt-3 text-gray-500 dark:text-gray-300">
+                아이디와 패스워드를 입력해주세요.
+              </p>
+            </div>
+
+            <div className="mt-8">
+              <form>
+                <div>
+                  <label
+                    form="id"
+                    className="block mb-2 text-sm text-gray-600 dark:text-gray-200"
+                  >
+                    아이디
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Your id"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
+
+                <div className="mt-6">
+                  <label
+                    form="password"
+                    className="text-sm text-gray-600 dark:text-gray-200"
+                  >
+                    비밀번호
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Your Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
+
+                <div className="mt-6">
+                  <button
+                    onClick={handleLogin}
+                    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  >
+                    접속 하기
+                  </button>
+                </div>
+              </form>
+              <a
+                href="#"
+                className="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline"
+              >
+                암호를 잊어버리셨나요?
+              </a>
+
+              <p className="mt-6 text-sm text-center text-gray-400">
+                아직 가입을 안하셨나요?{" "}
+                <a
+                  href="#"
+                  className="text-blue-500 focus:outline-none focus:underline hover:underline"
+                >
+                  가입하기
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
