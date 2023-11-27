a4afd27204fae9c14c0f22522afe0374862470b1
hrhrhrasd <gmleh1245@gmail.com>
2023-11-27 16:51:39 +0900

    memberEdit 후 경로

--- src/page/member/MemberEdit.js
+++ src/page/member/MemberEdit.js
@@ -14,7 +14,7 @@ import {
 } from "@chakra-ui/react";
 import axios from "axios";
 import {useNavigate, useSearchParams} from "react-router-dom";
-
+//TODO 수정 조건 추가하기
 function MemberEdit(props) {
     const [member, setMember] = useState(null);
     const [nickName, setNickName] = useState("");
