1247baf27dddc0f8077e38758af84bf13ba5f6f1
amz940 <dh_bug@naver.com>
2023-11-27 11:03:07 +0900

    drugStoreList 뷰 페이지 작성

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
-import {HomeLayout} from "./layout/HomeLayout";
-import React, {createContext, useEffect, useState} from "react";
-import {Drug} from "./page/Drug";
-import {Box} from "@chakra-ui/react";
-import {Hs} from "./page/Hs";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { HomeLayout } from "./layout/HomeLayout";
+import React, { createContext, useEffect, useState } from "react";
+import { Drug } from "./page/Drug";
+import { Box } from "@chakra-ui/react";
+import { Hs } from "./page/Hs";
 import axios from "axios";
-import DrugStoreWrite from "./page/DrugStore/DrugStoreWrite";
+import DsWrite from "./page/DrugStore/DsWrite";
+import { DsList } from "./page/DrugStore/DsList";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<HomeLayout/>}>
-            <Route path="drugStore" element={<DrugStoreWrite />} />
-            <Route path='hospital' element={<Hs/>}/>
-            <Route path="nutraceutical" element={<Drug/>}/>
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      <Route path="drugStore" element={<DsWrite />} />
+      <Route path="drugList" element={<DsList />} />
+      <Route path="hospital" element={<Hs />} />
+      <Route path="nutraceutical" element={<Drug />} />
+    </Route>,
+  ),
 );
 
-
 function App() {
-    const [key, setKey] = useState(null)
-    useEffect(() => {
-        // axios.get("/hospital")
-        //     .then(response => setKey(response.data));
-    }, []);
+  const [key, setKey] = useState(null);
+  useEffect(() => {
+    // axios.get("/hospital")
+    //     .then(response => setKey(response.data));
+  }, []);
 
-    return (
-        <Box>
-            <KakaoMapAppContext.Provider value={key}>
-                <RouterProvider router={router}/>
-            </KakaoMapAppContext.Provider>
-
-
-        </Box>
-    );
+  return (
+    <Box>
+      <KakaoMapAppContext.Provider value={key}>
+        <RouterProvider router={router} />
+      </KakaoMapAppContext.Provider>
+    </Box>
+  );
 }
 
-export const KakaoMapAppContext = createContext(null)
+export const KakaoMapAppContext = createContext(null);
 
 export default App;
