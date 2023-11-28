e7acc59716eb020783ec3106268e27b5fb08cbd7
hwani <klh481@naver.com>
2023-11-28 15:54:47 +0900

    front 사이드바, 네브바 위치조정중,, 팀원과 조율필요함..

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,13 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import { SideBar } from "./component/SideBar";
 
 export function Board() {
-    return <Outlet />;
+  return (
+    <Box>
+      <SideBar />
+      <Outlet />
+    </Box>
+  );
 }
