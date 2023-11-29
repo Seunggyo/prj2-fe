d9aa84b7b3b52e5ec214c36bd7a6bd19a39b6463
hwani <klh481@naver.com>
2023-11-29 20:23:57 +0900

    front 댓글 작성 후 댓글 리스트 업데이트기능(중복클릭처리가 안되게..)

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -8,11 +8,11 @@ import {
   Heading,
   Stack,
   StackDivider,
-  Textarea,
   Text,
+  Textarea,
 } from "@chakra-ui/react";
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function CommentForm({ boardId, isSubmitting, onSubmit }) {
   const [comment, setComment] = useState("");
@@ -22,64 +22,55 @@ function CommentForm({ boardId, isSubmitting, onSubmit }) {
   }
 
   return (
-    <div>
-      <Box>
-        <Textarea
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <Button onClick={handleSubmit} isDisabled={isSubmitting}>
-          쓰기
-        </Button>
-      </Box>
-    </div>
+    <Box>
+      <Textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+      <Button isDisabled={isSubmitting} onClick={handleSubmit}>
+        쓰기
+      </Button>
+    </Box>
   );
 }
 
 function CommentList({ commentList }) {
   return (
-    <div>
-      <Card>
-        <CardHeader>
-          <Heading size="md">댓글 리스트</Heading>
-        </CardHeader>
-        <CardBody>
-          <Stack divider={<StackDivider />} spacing="4">
-            {commentList.map((comment) => (
-              <Box>
-                <Flex justifyContent="space-between">
-                  <Heading size="xs">{comment.memberNickName}</Heading>
-                  <Text fontSize="xs">{comment.inserted}</Text>
-                </Flex>
-                <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
-                  {comment.comment}
-                </Text>
-              </Box>
-            ))}
-          </Stack>
-        </CardBody>
-      </Card>
-    </div>
+    <Card>
+      <CardHeader>
+        <Heading size="md">댓글 리스트</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          {commentList.map((comment) => (
+            <Box>
+              <Flex justifyContent="space-between">
+                <Heading size="xs">{comment.memberNickName}</Heading>
+                <Text fontSize="xs">{comment.inserted}</Text>
+              </Flex>
+              <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
+                {comment.comment}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
   );
 }
 
 export function CommentContainer({ boardId }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
+
   const [commentList, setCommentList] = useState([]);
 
   useEffect(() => {
-    const params = new URLSearchParams();
-    params.set("id", boardId);
-
     if (!isSubmitting) {
+      const params = new URLSearchParams();
+      params.set("id", boardId);
+
       axios
         .get("/api/comment/list?" + params)
-        .then((r) => setCommentList(r.data));
+        .then((response) => setCommentList(response.data));
     }
   }, [isSubmitting]);
-  // 댓글을 쓰고나서 isSumitting 상태가 변경될때마다 useEffect 안에 있는 내용이
-  // re-rendering 되는 것이 목적이기에 디펜던시(deps) 에 isSubmitting 들어감.
-  // 다만, isSubmitting 변경값이 false일 때에만 필요하기에 if조건문으로 감쌈.
 
   function handleSubmit(comment) {
     setIsSubmitting(true);
@@ -90,15 +81,13 @@ export function CommentContainer({ boardId }) {
   }
 
   return (
-    <div>
-      <Box>
-        <CommentForm
-          boardId={boardId}
-          isSubmitting={isSubmitting}
-          onSubmit={handleSubmit}
-        />
-        <CommentList commentList={commentList} />
-      </Box>
-    </div>
+    <Box>
+      <CommentForm
+        boardId={boardId}
+        isSubmitting={isSubmitting}
+        onSubmit={handleSubmit}
+      />
+      <CommentList boardId={boardId} commentList={commentList} />
+    </Box>
   );
 }
