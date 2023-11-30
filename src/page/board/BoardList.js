f9addc3003c32e6a326785dfa66d1cc69452f982
hwani <klh481@naver.com>
2023-11-30 10:09:21 +0900

    front 게시판 항목별 버튼추가..

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -29,7 +29,15 @@ export function BoardList() {
 
   return (
     <Box>
-      <h1>게시물 목록</h1>
+      <button className="relative h-12 w-40 overflow-hidden text-indigo-600 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-500 before:duration-300 before:ease-out hover:text-white  hover:before:h-40 hover:before:w-40 hover:before:opacity-80 font-dongle font-semibold font text-3xl">
+        <span className="relative z-10">전체 글</span>
+      </button>
+      <button className="relative h-12 w-40 overflow-hidden text-indigo-600 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-500 before:duration-300 before:ease-out hover:text-white  hover:before:h-40 hover:before:w-40 hover:before:opacity-80 font-dongle font-semibold font text-3xl">
+        <span className="relative z-10">인기 글</span>
+      </button>
+      <button className="relative h-12 w-40 overflow-hidden text-indigo-600 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-500 before:duration-300 before:ease-out hover:text-white  hover:before:h-40 hover:before:w-40 hover:before:opacity-80 font-dongle font-semibold font text-3xl">
+        <span className="relative z-10">공 지</span>
+      </button>
       <Box>
         <Table>
           <Thead>
