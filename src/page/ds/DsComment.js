b8e084da4f4553bce02e67428824e5e197f797de
amz940 <dh_bug@naver.com>
2023-12-02 21:17:18 +0900

    댓글 수정, 삭제 추가

--- src/page/ds/DsComment.js
+++ src/page/ds/DsComment.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import {
   Box,
   Button,
@@ -23,6 +23,7 @@ import {
 } from "@chakra-ui/react";
 import { LoginContext } from "../../component/LoginProvider";
 import axios from "axios";
+import { DeleteIcon, EditIcon, NotAllowedIcon } from "@chakra-ui/icons";
 
 function CommentForm({ businessId, isSubmitting, onSubmit }) {
   const [comment, setComment] = useState("");
@@ -41,20 +42,109 @@ function CommentForm({ businessId, isSubmitting, onSubmit }) {
   );
 }
 
-// function CommentItem({ DsComment, isSubmitting, setIsSubmitting }) {
-//   const [isEditing, setIsEditing] = useState(false);
-//   const [commentEdited, setCommentEdited] = useState(DsComment.comment);
-//   return (
-//     <Box>
-//       <Flex justifyContent="space-between">
-//         <Heading>{DsComment.memberNickName}</Heading>
-//         <Text>{DsComment.ago}</Text>
-//       </Flex>
-//     </Box>
-//   );
-// }
+function CommentItem({
+  DsComment,
+  isSubmitting,
+  setIsSubmitting,
+  onDeleteModalOpen,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [commentEdited, setCommentEdited] = useState(DsComment.comment);
 
-function CommentList({ commentList }) {
+  const toast = useToast();
+
+  const { hasAccess } = useContext(LoginContext);
+
+  function handleCommentSubmit() {
+    // setIsSubmitting(true);
+
+    axios
+      .put("/api/ds/comment/edit", { id: DsComment.id, comment: commentEdited })
+      .then(() => {
+        toast({
+          description: "댓글이 수정되었습니다.",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+          toast({
+            description: "권한이 없습니다.",
+            status: "warning",
+          });
+        }
+
+        if (error.response.status === 400) {
+          toast({
+            description: "입력값을 확인해주세요.",
+            status: "warning",
+          });
+        }
+      })
+      .finally(() => {
+        // TODO : setIsSubmitting 오류 해결해야함
+        // setIsSubmitting(false);
+        setIsEditing(false);
+      });
+  }
+
+  return (
+    <Box>
+      <Flex justifyContent="space-between">
+        <Heading>{DsComment.memberNickName}</Heading>
+        <Text>{DsComment.inserted}</Text>
+      </Flex>
+      <Flex>
+        <Box flex={1}>
+          <Text>{DsComment.comment}</Text>
+          {isEditing && (
+            <Box>
+              <Textarea
+                value={commentEdited}
+                onChange={(e) => setCommentEdited(e.target.value)}
+              />
+              <Button
+                isDisabled={isSubmitting}
+                colorScheme="blue"
+                onClick={handleCommentSubmit}
+              >
+                저장
+              </Button>
+            </Box>
+          )}
+        </Box>
+
+        {hasAccess(DsComment.memberId) && (
+          <Box>
+            {isEditing || (
+              <Button colorScheme="purple" onClick={() => setIsEditing(true)}>
+                <EditIcon />
+              </Button>
+            )}
+            {isEditing && (
+              <Button colorScheme="gray" onClick={() => setIsEditing(false)}>
+                <NotAllowedIcon />
+              </Button>
+            )}
+            <Button
+              onClick={() => onDeleteModalOpen(DsComment.id)}
+              colorScheme="red"
+            >
+              <DeleteIcon />
+            </Button>
+          </Box>
+        )}
+      </Flex>
+    </Box>
+  );
+}
+
+function CommentList({
+  commentList,
+  isSubmitting,
+  setIsSubmitting,
+  onDeleteModalOpen,
+}) {
   return (
     <Card>
       <CardHeader>
@@ -63,13 +153,13 @@ function CommentList({ commentList }) {
       <CardBody>
         <Stack divider={<StackDivider />} spacing="4">
           {commentList.map((DsComment) => (
-            <Box>
-              <Flex>
-                <Heading>{DsComment.memberId}</Heading>
-                <Text>{DsComment.inserted}</Text>
-              </Flex>
-              <Text>{DsComment.comment}</Text>
-            </Box>
+            <CommentItem
+              key={DsComment.id}
+              isSubmitting={isSubmitting}
+              setIsSubmitting={setIsSubmitting}
+              DsComment={DsComment}
+              onDeleteModalOpen={onDeleteModalOpen}
+            />
           ))}
         </Stack>
       </CardBody>
@@ -84,6 +174,9 @@ export function DsComment({ businessId }) {
   const [commentList, setCommentList] = useState([]);
 
   const { isOpen, onClose, onOpen } = useDisclosure();
+
+  // 임시로 값을 저장하는 용도로 사용할려고 씀
+  const commentIdRef = useRef(0);
 
   const toast = useToast();
 
@@ -119,9 +212,40 @@ export function DsComment({ businessId }) {
       .finally(() => setIsSubmitting(false));
   }
 
-  // function handleDeleteComment() {
-  //   setIsSubmitting(true);
-  // }
+  function handleDeleteComment() {
+    setIsSubmitting(true);
+    axios
+      .delete("/api/ds/comment/" + commentIdRef.current)
+      .then(() => {
+        toast({
+          description: "댓글이 삭제되었습니다.",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+          toast({
+            description: "권한이 없습니다.",
+            status: "warning",
+          });
+        } else {
+          toast({
+            description: "댓글 삭제 중 문제가 발생했습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => {
+        onClose();
+        setIsSubmitting(false);
+      });
+  }
+
+  function handleDeleteModalOpen(id) {
+    commentIdRef.current = id;
+    // 삭제 모달 열기
+    onOpen();
+  }
 
   return (
     <Box>
@@ -139,27 +263,27 @@ export function DsComment({ businessId }) {
         isSubmitting={isSubmitting}
         setIsSubmitiing={setIsSubmitting}
         commentList={commentList}
-        // ondeleteModal={handleOnDeleteModal}
+        onDeleteModalOpen={handleDeleteModalOpen}
       />
-      {/*삭제 모달*/}
-      {/*<Modal isOpen={isOpen} onClose={onClose}>*/}
-      {/*  <ModalOverlay />*/}
-      {/*  <ModalContent>*/}
-      {/*    <ModalHeader>삭제 확인</ModalHeader>*/}
-      {/*    <ModalCloseButton />*/}
-      {/*    <ModalBody>삭제 하시겠습니까?</ModalBody>*/}
-      {/*    <ModalFooter>*/}
-      {/*      <Button*/}
-      {/*        onClick={handleDeleteComment}*/}
-      {/*        colorScheme="red"*/}
-      {/*        isDisabled={isSubmitting}*/}
-      {/*      >*/}
-      {/*        삭제*/}
-      {/*      </Button>*/}
-      {/*      <Button onClick={onClose}>닫기</Button>*/}
-      {/*    </ModalFooter>*/}
-      {/*  </ModalContent>*/}
-      {/*</Modal>*/}
+      삭제 모달
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>삭제 하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button
+              onClick={handleDeleteComment}
+              colorScheme="red"
+              isDisabled={isSubmitting}
+            >
+              삭제
+            </Button>
+            <Button onClick={onClose}>닫기</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
