ef668f6aa3abf04a3ba2a228e58d8d0c23a4278d
amz940 <dh_bug@naver.com>
2023-12-02 22:43:49 +0900

    좋아요, 댓글 수 표시

--- src/page/ds/DsList.js
+++ src/page/ds/DsList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
+  Badge,
   Box,
   Button,
   Heading,
@@ -18,6 +19,9 @@ import {
   ZoomControl,
 } from "react-kakao-maps-sdk";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ChatIcon } from "@chakra-ui/icons";
 
 export function DsList() {
   const [dsList, setDsList] = useState([]);
@@ -45,6 +49,10 @@ export function DsList() {
             <Thead>
               <Tr>
                 <Th>약국이름</Th>
+                <Th>
+                  <FontAwesomeIcon icon={faHeart} color="red" />
+                </Th>
+                <Th>댓글 수</Th>
                 <Th>전화번호</Th>
                 <Th>약국주소</Th>
               </Tr>
@@ -57,6 +65,8 @@ export function DsList() {
                   onClick={() => navigate("/ds/view/" + ds.id)}
                 >
                   <Td>{ds.name}</Td>
+                  <Td>{ds.likeCount}</Td>
+                  <Td>{ds.commentCount > 0 && ds.commentCount}</Td>
                   <Td>{ds.phone}</Td>
                   <Td>{ds.address}</Td>
                 </Tr>
