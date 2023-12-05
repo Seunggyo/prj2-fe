518e42bf075ff6b7908bb823d305cb8b1f7372bd
som42 <zzzoo42@naver.com>
2023-12-05 19:15:24 +0900

    밑에 게시물 페이지로 하나하나 하는거 오류 나니깐 내일 다시 해보기

--- src/page/drug/DrugPagination.js
+++ src/page/drug/DrugPagination.js
@@ -1,7 +1,58 @@
+import { Box, Button } from "@chakra-ui/react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+function PageButton({ variant, pageNumber, children }) {
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    params.set("p", pageNumber);
+    navigate("/?" + params);
+  }
+
+  return (
+    <Button variant={variant} onClick={handleClick}>
+      {children}
+    </Button>
+  );
+}
+
 export function DrugPagination({ pageInfo }) {
-  const pageNumber = [];
+  const pageNumbers = [];
+  const navigate = useNavigate();
 
-  // for (let i = pageInfo.st)
+  for (let i = pageInfo.startPageNumber; i <= pageInfo.endPageNumber; i++) {
+    pageNumbers.push(i);
+  }
 
-  return;
+  return (
+    <Box>
+      {pageInfo.prevPageNumber && (
+        <PageButton variant="ghost" pageNumber={pageInfo.prevPageNumber}>
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </PageButton>
+      )}
+
+      {pageNumbers.map((pageNumber) => (
+        <PageButton
+          key={pageNumber}
+          variant={
+            pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
+          }
+          pageNumber={pageNumber}
+        >
+          {pageNumber}
+        </PageButton>
+      ))}
+
+      {pageInfo.nextPageNumber && (
+        <PageButton variant="ghost" pageNumber={pageInfo.nextPageNumber}>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </PageButton>
+      )}
+    </Box>
+  );
 }
