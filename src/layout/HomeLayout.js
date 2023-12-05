80d3b262f4e4e2b14c5aac3c6334365a4fc6d453
amz940 <dh_bug@naver.com>
2023-12-05 17:11:54 +0900

    안쓰는 임포트 삭제

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -2,7 +2,6 @@ import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../component/Navbar";
 import React from "react";
-import { SideBar } from "../component/SideBar";
 
 export function HomeLayout() {
   return (
