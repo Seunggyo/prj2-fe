fb6abbd124ba791c9f19ed5a8d4817ef2e3e9f2e
hwani <klh481@naver.com>
2023-12-06 14:54:29 +0900

    front homelayout 대충 완성...

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { NavBar } from "../component/Navbar";
 import React from "react";
 import { SideBar } from "./SideBar";
+import { SideBarExp } from "./SideBarExp";
 
 export function HomeLayout() {
   return (
@@ -11,6 +12,9 @@ export function HomeLayout() {
         <NavBar />
       </Box>
       <div>
+        <Box>
+          <SideBarExp />
+        </Box>
         <Box>
           <SideBar />
         </Box>
