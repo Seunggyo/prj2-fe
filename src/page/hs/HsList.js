c899acd89b97747c040d95a672e70e693084ccbf
amz940 <dh_bug@naver.com>
2023-12-15 11:42:31 +0900

    어드민 list 따로 빼고 검색 및 페이징 추가

--- src/page/hs/HsList.js
+++ src/page/hs/HsList.js
@@ -1,5 +1,5 @@
-import {useNavigate, useSearchParams} from "react-router-dom";
-import React, {useContext, useEffect, useState} from "react";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import {
   Badge,
@@ -7,6 +7,7 @@ import {
   Button,
   Flex,
   Heading,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -14,6 +15,7 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Select,
   Spacer,
   Table,
   Tbody,
@@ -24,46 +26,138 @@ import {
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
-import {LoginContext} from "../../component/LoginProvider";
+import { LoginContext } from "../../component/LoginProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+function PageButton({ variant, pageNumber, children }) {
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    params.set("p", pageNumber);
+
+    navigate("?" + params);
+  }
+
+  return (
+    <Button variant={variant} onClick={handleClick}>
+      {children}
+    </Button>
+  );
+}
+
+function Pagination({ pageInfo }) {
+  const pageNumbers = [];
+  const navigate = useNavigate();
+
+  for (let i = pageInfo.startPageNumber; i <= pageInfo.endPageNumber; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <Box>
+      {/* 뒤로가기*/}
+      {pageInfo.prevPageNumber && (
+        <PageButton variant="ghost" pageNumber={pageInfo.prevPageNumber}>
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </PageButton>
+      )}
+
+      {pageNumbers.map((pageNumber) => (
+        <PageButton
+          key={pageNumber}
+          variant={
+            pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
+          }
+          pageNumber={pageNumber}
+        >
+          {pageNumber}
+        </PageButton>
+      ))}
+
+      {/*앞으로 가기*/}
+      {pageInfo.nextPageNumber && (
+        <PageButton variant="ghost" pageNumber={pageInfo.nextPageNumber}>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </PageButton>
+      )}
+    </Box>
+  );
+}
+
+function SearchComponent() {
+  const [keyword, setKeyword] = useState("");
+  const [list, setList] = useState("all");
+  const navigate = useNavigate();
+
+  function handleSearch() {
+    const params = new URLSearchParams();
+
+    params.set("k", keyword);
+    params.set("l", list);
+
+    navigate("?" + params);
+  }
+
+  return (
+    <Box>
+      <Flex>
+        <Select value={list} onChange={(e) => setList(e.target.value)}>
+          <option value="all">전체</option>
+          <option value="name">이름</option>
+          <option value="address">주소</option>
+          <option value="medicalCourse">진료과목</option>
+        </Select>
+        <Input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+        <Button onClick={handleSearch}>검색</Button>
+      </Flex>
+    </Box>
+  );
+}
 
 export function HsList() {
-    const {isAuthenticated, isAdmin} = useContext(LoginContext);
-    const navigate = useNavigate();
-    const toast = useToast();
-    const [params] = useSearchParams();
+  const { isAuthenticated, isAdmin } = useContext(LoginContext);
 
-    const [list, setList] = useState([]);
-    const {isOpen, onClose, onOpen} = useDisclosure();
+  const navigate = useNavigate();
+  const toast = useToast();
+  const [params] = useSearchParams();
+  const [pageInfo, setPageInfo] = useState("");
 
-    const [position, setPosition] = useState();
+  const [list, setList] = useState([]);
+  const location = useLocation();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   useEffect(() => {
-    axios.get("/api/hospital/list?" + params).then((r) => setList(r.data.list));
-  }, []);
+    axios.get("/api/hospital/listAdmin?" + params).then((r) => {
+      setList(r.data.list);
+      setPageInfo(r.data.pageInfo);
+    });
+  }, [location]);
 
-    function handleDeleteClick() {
-        if (!position) {
-            return;
-        }
-        axios
-            .delete(`/api/hospital/delete/${position}`)
-            .then(() => {
-                toast({
-                    description: "삭제가 완료되었습니다.",
-                    status: "success",
-                });
-            })
-            .catch(() => {
-                toast({
-                    description: "실패하였습니다.",
-                    status: "error",
-                });
-            })
-            .finally(() => {
-                setPosition(null);
-                onClose();
-            });
+  function handleDeleteClick() {
+    if (!position) {
+      return;
     }
+    axios
+      .delete(`/api/hospital/delete/${position}`)
+      .then(() => {
+        toast({
+          description: "삭제가 완료되었습니다.",
+          status: "success",
+        });
+      })
+      .catch(() => {
+        toast({
+          description: "실패하였습니다.",
+          status: "error",
+        });
+      })
+      .finally(() => {
+        setPosition(null);
+        onClose();
+      });
+  }
 
   return (
     <Box>
@@ -128,6 +222,8 @@ export function HsList() {
           </Table>
         </Box>
       </Box>
+      <SearchComponent />
+      <Pagination pageInfo={pageInfo} />
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
