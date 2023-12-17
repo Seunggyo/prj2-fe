12ddc7c7a83a92396123bff8efde4f7893310632
hrhrhrasd <gmleh1245@gmail.com>
2023-12-17 17:14:36 +0900

    주문내역, 상세 css

--- src/page/member/PaymentDetail.js
+++ src/page/member/PaymentDetail.js
@@ -17,11 +17,11 @@ export function PaymentDetail() {
     return <Spinner />;
   }
   return (
-    <Box maxWidth={"800px"} marginLeft={"100px"}>
+    <Box maxWidth={"1000px"} marginLeft={"auto"} marginRight={"auto"} px={4}>
       {/* 장바구니 css*/}
       <Center>
         <Box marginTop="20px">
-          <div className="col-span-1 bg-white lg:block hidden">
+          <div className="col-span-1 bg-white lg:block hidden shadow-lg rounded p-6">
             <Flex>
               <Box>
                 {orderList.map((order) => (
@@ -29,30 +29,32 @@ export function PaymentDetail() {
                     className="py-6 border-b space-y-6 px-8 w-auto"
                     key={order.id}
                   >
-                    <li className="grid grid-cols-6 gap-2 border-b-1">
+                    <li className="grid grid-cols-6 gap-4 border-b-1">
                       <div className="col-span-1 self-center">
                         <Image
                           src={order.url}
                           alt={order.url}
-                          className="rounded w-full"
+                          className="rounded w-full h-auto shadow-md"
                         />
                       </div>
                       <div className="flex flex-col col-span-3 pt-2">
-                        <span className="text-gray-600 text-md font-semi-bold">
-                          상품명 : {order.drugName}
+                        <span
+                          className="text-gray-600 text-md"
+                          style={{ fontWeight: "500" }}
+                        >
+                          상품명: {order.drugName}
                         </span>
-                        <span className="text-gray-400 text-sm inline-block pt-2">
-                          기능 : {order.func}
+                        <span className="text-gray-400 text-sm pt-2">
+                          기능: {order.func}
                         </span>
                       </div>
-                      <br />
                       <div className="col-span-2 pt-3">
-                        <div className="flex items-center space-x-2 text-sm justify-between">
+                        <div className="flex flex-col items-end text-sm">
                           <span className="text-gray-400">
-                            개수 : {order.quantity}
+                            개수: {order.quantity}
                           </span>
-                          <span className="text-pink-400 font-semibold inline-block">
-                            가격 : {order.total}
+                          <span className="text-pink-400 font-semibold">
+                            가격: {order.total}
                           </span>
                         </div>
                       </div>
@@ -60,48 +62,74 @@ export function PaymentDetail() {
                   </ul>
                 ))}
               </Box>
-              <Center marginBottom={"56px"} marginRight={"80px"}>
-                <section>
+              <Center
+                marginBottom={"56px"}
+                style={{ alignItems: "flex-start" }}
+              >
+                <section style={{ paddingLeft: "10px" }}>
                   {/*주문자*/}
-                  <h2 className="uppercase tracking-wide text-lg font-semibold text-gray-700 my-2 mt-2">
-                    배송지 정보
-                  </h2>
-                  <fieldset className="mb-3 bg-white shadow-lg rounded text-gray-600">
-                    <label className="flex border-b border-gray-200 h-12 py-3 items-center">
-                      <span className="text-right px-2">이름</span>
-                      <input
-                        name="name"
-                        className="focus:outline-none px-3"
-                        value={"수령인 : " + location.state.order.deliveryName}
-                        readOnly
-                      />
+                  <fieldset
+                    className="mb-3 bg-white shadow-lg rounded p-6 text-gray-600"
+                    style={{ minWidth: "300px" }}
+                  >
+                    <h1
+                      className="uppercase tracking-wide text-lg font-semibold text-gray-700 my-2 mt-2"
+                      style={{ fontSize: "2rem", paddingBottom: "10px" }}
+                    >
+                      배송지 정보
+                    </h1>
+                    <label className="flex border-b border-gray-200 py-2 items-center">
+                      <div
+                        className="text-left "
+                        style={{ whiteSpace: "nowrap", fontWeight: "600" }}
+                      >
+                        수령인 :
+                      </div>
+                      <div
+                        className="text-left px-2"
+                        style={{ whiteSpace: "nowrap" }}
+                      >
+                        {location.state.order.deliveryName}
+                      </div>
                     </label>
-                    <label className="flex border-b border-gray-200 h-12 py-3 items-center">
-                      <span className="text-right px-2">번호</span>
-                      <input
-                        name="phone"
-                        type="phone"
-                        className="focus:outline-none px-3"
-                        value={"번호 : " + location.state.order.deliveryPhone}
-                        readOnly
-                      />
+                    <label className="flex border-b border-gray-200 py-2 items-center">
+                      <div
+                        className="text-left "
+                        style={{ whiteSpace: "nowrap", fontWeight: "600" }}
+                      >
+                        번호 :
+                      </div>
+                      <div
+                        className="text-left px-2"
+                        style={{ whiteSpace: "nowrap" }}
+                      >
+                        {location.state.order.deliveryPhone}
+                      </div>
                     </label>
-                    <label className="flex border-b border-gray-200 h-12 py-3 items-center">
-                      <span className="text-right px-2">email</span>
-                      <input
-                        name="address"
-                        className="focus:outline-none px-3"
-                        value={"주소 : " + location.state.order.deliveryAddress}
-                        readOnly
-                      />
+                    <label className="flex border-b border-gray-200 py-2 items-center">
+                      <div
+                        className="text-left "
+                        style={{ whiteSpace: "nowrap", fontWeight: "600" }}
+                      >
+                        주소 :
+                      </div>
+                      <div className="text-left px-2 ">
+                        {location.state.order.deliveryAddress}
+                      </div>
                     </label>
-                    <label className="flex border-b border-gray-200 h-12 py-3 items-center">
-                      <span className="text-right px-2">주소</span>
-                      <input
-                        name="comment"
-                        className="focus:outline-none px-3"
-                        value={"메모 : " + location.state.order.deliveryComment}
-                      />
+                    <label className="flex border-b border-gray-200 py-2 items-center">
+                      <div
+                        className="text-left"
+                        style={{ whiteSpace: "nowrap", fontWeight: "600" }}
+                      >
+                        메모 :
+                      </div>
+                      <div
+                        className="text-left px-2"
+                        style={{ whiteSpace: "nowrap" }}
+                      >
+                        {location.state.order.deliveryComment}
+                      </div>
                     </label>
                   </fieldset>
                 </section>
--- src/page/member/PaymentHistory.js
+++ src/page/member/PaymentHistory.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import {
   Box,
+  Button,
   Img,
   Spinner,
   Table,
@@ -33,29 +34,74 @@ export function PaymentHistory() {
     });
   };
   return (
-    <Box>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>사진</Th>
-            <Th>이름</Th>
-            <Th>가격</Th>
-            <Th>주문시각</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {orderList.map((order) => (
-            <Tr key={order.orderId} onClick={() => handleHistoryClick(order)}>
-              <Td>
-                <Img src={order.url} alt={order.url} w={"50px"} />
-              </Td>
-              <Td>{order.orderName}</Td>
-              <Td>{order.amount}</Td>
-              <Td>{order.inserted}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </Box>
+    <div className="container mx-auto p-4">
+      <div
+        className="flex -mx-2 overflow-hidden"
+        style={{ flexDirection: "column", alignItems: "center" }}
+      >
+        {orderList.map((order) => (
+          <div
+            key={order.orderId}
+            className="w-full   p-2"
+            style={{ maxWidth: "900px" }}
+          >
+            <div
+              className="flex bg-white border border-gray-300 p-4"
+              style={{ flexDirection: "row" }}
+            >
+              <div className="text-center">
+                <img
+                  src={order.url}
+                  alt={order.url}
+                  className="w-36 h-36 object-cover mx-auto"
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  flexDirection: "row",
+                  width: "100%",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    paddingLeft: "20px",
+                    paddingBottom: "10px",
+                    paddingTop: "10px",
+                  }}
+                >
+                  <div
+                    style={{
+                      flex: "1",
+                      fontSize: "1.5rem",
+                      justifyContent: "space-between",
+                      fontWeight: "500",
+                    }}
+                  >
+                    {order.orderName}
+                  </div>
+                  <div className="font-bold" style={{ fontSize: "1.2rem" }}>
+                    {order.amount}원
+                  </div>
+                  <div className="text-gray-500">{order.inserted}</div>
+                </div>
+                <div style={{ paddingTop: "10px", paddingRight: "10px" }}>
+                  <Button
+                    style={{ justifyContent: "flex-end" }}
+                    onClick={() => handleHistoryClick(order)}
+                  >
+                    상세보기
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
