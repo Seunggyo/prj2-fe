1b67b54283f81fe0a334aac0866de65ef0e7ced2
hrhrhrasd <gmleh1245@gmail.com>
2023-12-21 10:27:38 +0900

    주문 페이지 수정

--- src/page/drug/DrugBuy.js
+++ src/page/drug/DrugBuy.js
@@ -210,8 +210,8 @@ export function DrugBuy() {
                 <label class="flex border-b border-gray-200 h-12 py-3 items-center">
                   <Image
                     w={"50px"}
-                    src={location.state.url}
-                    alt={location.state.url}
+                    src={location.state.cartList[0].url}
+                    alt={location.state.cartList[0].url}
                   />
                   <span>{location.state.orderName}</span>
                 </label>
@@ -224,6 +224,47 @@ export function DrugBuy() {
           >
             {location.state.amount}원 결제
           </button>
+        </div>
+        <div className="col-span-1 bg-white lg:block hidden">
+          <h1 className="py-6 border-b-2 text-xl text-gray-600 px-8">
+            주문 상품
+          </h1>
+          <div className="px-8 border-b"></div>
+          <ul className="py-6 border-b space-y-6 px-8">
+            {location.state.cartList.map((cart) => (
+              <li className="grid grid-cols-6 gap-2 border-b-1">
+                <div className="col-span-1 self-center">
+                  <img
+                    src={cart.url}
+                    alt={cart.url}
+                    className="rounded w-full"
+                  />
+                </div>
+                <div className="flex flex-col col-span-3 pt-2">
+                  <span className="text-gray-600 text-md font-semi-bold">
+                    {cart.drugName}
+                  </span>
+                  <span className="text-gray-400 text-sm inline-block pt-2">
+                    {cart.func}
+                  </span>
+                </div>
+                <div className="col-span-2 pt-3">
+                  <div className="flex items-center space-x-2 text-sm justify-between">
+                    <span className="text-gray-400">
+                      수량 : {cart.quantity}개
+                    </span>
+                    <span className="text-pink-400 font-semibold inline-block">
+                      {cart.total}원
+                    </span>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="font-semibold text-xl px-8 flex justify-between py-8 text-gray-600">
+            <span>Total</span>
+            <span>{location.state.amount} 원</span>
+          </div>
         </div>
       </div>
     </Box>
